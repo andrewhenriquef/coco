78d6f3e4aff6e4e746848e68b2fee02624758a78
Andrew Henrique <andrewhenriquef@gmail.com>
2015-08-01 00:44:09 -0300

    customs validates add, validade size of capacity and validate baggage_allowance

--- coco/app/models/seat.rb
+++ coco/app/models/seat.rb
@@ -1,2 +1,19 @@
 class Seat < ActiveRecord::Base
+    belongs_to :flight
+    validate :bagagem
+    
+    def bagagem
+        
+        if baggage > flight.baggage_allowance
+            
+            errors.add(:baggage, "Você tm bagagem em excesso!")
+            
+        end
+        
+        if flight.seats.size >= flight.capacity
+            errors.add(:capacity, "As reservas para este voo estao esgotadas")
+        end
+        
+    end
+    
 end
